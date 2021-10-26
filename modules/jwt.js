9fbd32f4254bcfba924c60d18aa5c70864da67a5
IlhomMusayev <ilhommusayev2001@gmail.com>
2021-10-26 16:24:57 +0500

    CreateToken created

--- modules/jwt.js
+++ modules/jwt.js
@@ -1,10 +1,12 @@
-import JOI from 'joi'
-import CustomError from '../helpers/costomError.js'
-export default class Validations {
-    static async SignInValidation(data, CustomError){
-        return await JOI.object({
-            username: JOI.string().required().min(3).max(64).error(new CustomError("Name is invalid")),
-            password: JOI.string().required().min(5).error(new CustomError("Password is invalid"))
-        }).validateAsync(data)
+import jwt from 'jsonwebtoken'
+
+export default class JWT{
+    static async createToken(user) {
+        return jwt.sign(user, process.env.JWT_SECRET, {
+            expiresIn: '1d'
+        })
+    }  
+    static async verifyToken(token) {
+        return jwt.verify(token, process.env.JWT_SECRET)
     }
 }
